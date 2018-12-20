806465897f589d6248ccf9f010e3e77b33b8f8d9
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 17:23:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -30,11 +30,24 @@
 def key_for_min_value(name_hash)
   lowest_number = {}
   lowest_key =  0 
-  current_lowest = 0 
+  current_lowest = 0
+  current_value = 0 
   if name_hash.empty? ==  true 
     return nil 
   else 
     name_hash.each do |key, value|
+       if lowest_number.empty? == true 
+          lowest_number = {key => value}
+          current_lowest = value
+        elsif current_lowest > current_value
+            lowest_number = {key => value}
+            current_lowest = value
+        end
+      
+      
+      
+      
+      
       
   
 end
